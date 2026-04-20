ff39419a7988790ca74773e9fc66f5163b3915fb
Martin <coxmartin877@gmail.com>
2026-04-19 22:39:51 -0600

    Formateo de lineas

--- api.js
+++ api.js
@@ -2,13 +2,9 @@ const express = require('express');
 const routeImagenes = express.Router();
 const { obtenerImagenes } = require('./services/ImageKit');
 
-const INFORMACION_PERMITIDA = ["gimnospermas", "briofitas", "angiospermas"]
-
 routeImagenes.get('/v1/imagenes/:especie', async (request, response) => {
     const especie = request.params.especie  
-
-    if(!INFORMACION_PERMITIDA.includes(especie)) return response.status(401).json({error: "Formato no valido"});
-
+    
     try {
         const imagenes = await obtenerImagenes(especie);
         response.status(200).json({ imagenes });
